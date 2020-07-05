fe31b58a89227e0ed215644e2e0cf03c94947b3f
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-07-05 15:22:19 +0700

    add homeWork file file

---
+++ HomeWork/insertSort.php
@@ -0,0 +1,16 @@
+<?php
+function insertSortByMax(array $arr) : array
+{
+    for ($i = 0; $i < count($arr); $i++) {
+        $val = $arr[$i];
+        $j = $i - 1;
+        while ($j >= 0 && $arr[$j] > $val) {
+            $arr[$j + 1] = $arr[$j];
+            $j--;
+        }
+        $arr[$j + 1] = $val;
+    }
+    return $arr;
+}
+echo "<pre>";
+print_r(insertSortByMax( [5, -4, 3, 7, 2, 1, 0, 8, 9, 2]));
